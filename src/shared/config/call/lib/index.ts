1fd672537076fd86bb7097223c0d053a864da209
pxmpkeen <pxmpkeen@gmail.com>
2025-12-20 03:10:08 +0400

    feat(app): Export specific types and utility functions for improved modularity

--- src/shared/config/call/lib/index.ts
+++ src/shared/config/call/lib/index.ts
@@ -1,2 +1,16 @@
-export * from "./_types";
-export * from "./_utils";
+export type {
+    ErrorDetail,
+    ErrorPayload,
+    FileResponse,
+    HttpMethod,
+    RequestOptions,
+} from "./_types";
+export { CallError } from "./_types";
+export {
+    doFetch,
+    extractFile,
+    getErrorDetail,
+    isErrorPayload,
+    isFile,
+    parseToFormData,
+} from "./_utils";
